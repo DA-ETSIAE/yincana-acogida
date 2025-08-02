4860d2cbe637490b6295141e3b9b72bfd7b169b4
Javier Lorenzana Orts <104021101+javilorts@users.noreply.github.com>
2025-08-02 23:17:29 +0530

    Update preguntas.js

--- preguntas.js
+++ preguntas.js
@@ -1,10 +1,45 @@
 const est2numb = new Map([
-        ["Hangar", 0],
-        ["Edificio A", 1]]);
+        ["Avión", 1],
+        ["Hall Edifico C", 2],
+        ["Hangar", 3],
+        ["Aula Iberia", 4],
+        ["Centro de Cálculo", 5],
+        ["Aula Airbus", 6],
+        ["Edificio B", 7],
+        ["Secretaría", 8],
+        ["Helicóptero", 9],
+        ["Aula Miguel Ángel Barcala", 10],
+        ["E3", 11],
+        ["Cafetería", 12],
+        ["Asociaciones", 13],
+        ["Publicaciones", 14],
+        ["E1", 15],
+        ["Claustro", 16],
+        ["E2", 17],
+        ["A015", 18],
+        ["Biblioteca A", 19]]);
 
 
-var preguntas_hangar = ['Pregunta1', 'Pregunta2', 'Pregunta3'];
-var preguntas_edifa = ['PreguntaX', 'PreguntaY', 'PreguntaZ'];
+var preguntas_avion = ['Pregunta1'];
+var preguntas_hallc = ['Pregunta2'];
+var preguntas_hangar = ['Pregunta3'];
+var preguntas_iberia = ['Pregunta4'];
+var preguntas_cdc = ['Pregunta5'];
+var preguntas_airbus = ['Pregunta6'];
+var preguntas_edifb = ['Pregunta7'];
+var preguntas_secretaria = ['Pregunta8'];
+var preguntas_helicoptero = ['Pregunta9'];
+var preguntas_barcala = ['Pregunta10'];
+var preguntas_e3 = ['Pregunta11'];
+var preguntas_cafeteria = ['Pregunta12'];
+var preguntas_asociaciones = ['Pregunta13'];
+var preguntas_publicaciones = ['Pregunta14'];
+var preguntas_e1 = ['Pregunta15'];
+var preguntas_claustro = ['Pregunta16'];
+var preguntas_e2 = ['Pregunta17'];
+var preguntas_a015 = ['Pregunta18'];
+var preguntas_biblioteca = ['Pregunta19'];
+
 
 var matriz_preguntas = [preguntas_hangar, preguntas_edifa];
 
