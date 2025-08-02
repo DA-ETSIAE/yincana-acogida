372396b53d806f65065b5b67817d5c43b01b35ea
ivan-moya-o <ivan.mercurio80@gmail.com>
2025-08-02 21:41:34 +0200

    Update preguntas.js

--- preguntas.js
+++ preguntas.js
@@ -1,23 +1,24 @@
 const est2numb = new Map([
-        ["Avión", 1],
-        ["Hall Edifico C", 2],
-        ["Hangar", 3],
-        ["Aula Iberia", 4],
-        ["Centro de Cálculo", 5],
-        ["Aula Airbus", 6],
-        ["Edificio B", 7],
-        ["Secretaría", 8],
-        ["Helicóptero", 9],
-        ["Aula Miguel Ángel Barcala", 10],
-        ["E3", 11],
-        ["Cafetería", 12],
-        ["Asociaciones", 13],
-        ["Publicaciones", 14],
-        ["E1", 15],
-        ["Claustro", 16],
-        ["E2", 17],
-        ["A015", 18],
-        ["Biblioteca A", 19]]);
+    ["Avión", 1],
+    ["Hall Edifico C", 2],
+    ["Hangar", 3],
+    ["Aula Iberia", 4],
+    ["Centro de Cálculo", 5],
+    ["Aula Airbus", 6],
+    ["Edificio B", 7],
+    ["Secretaría", 8],
+    ["Helicóptero", 9],
+    ["Aula Miguel Ángel Barcala", 10],
+    ["E3", 11],
+    ["Cafetería", 12],
+    ["Asociaciones", 13],
+    ["Publicaciones", 14],
+    ["E1", 15],
+    ["Claustro", 16],
+    ["E2", 17],
+    ["A015", 18],
+    ["Biblioteca A", 19]
+]);
 
 
 var preguntas_avion = ['Pregunta1'];
@@ -42,43 +43,37 @@ var preguntas_biblioteca = ['Pregunta19'];
 
 
 var matriz_preguntas = [
-  preguntas_avion,
-  preguntas_hallc,
-  preguntas_hangar,
-  preguntas_iberia,
-  preguntas_cdc,
-  preguntas_airbus,
-  preguntas_edifb,
-  preguntas_secretaria,
-  preguntas_helicoptero,
-  preguntas_barcala,
-  preguntas_e3,
-  preguntas_cafeteria,
-  preguntas_asociaciones,
-  preguntas_publicaciones,
-  preguntas_e1,
-  preguntas_claustro,
-  preguntas_e2,
-  preguntas_a015,
-  preguntas_biblioteca,
+    preguntas_avion,
+    preguntas_hallc,
+    preguntas_hangar,
+    preguntas_iberia,
+    preguntas_cdc,
+    preguntas_airbus,
+    preguntas_edifb,
+    preguntas_secretaria,
+    preguntas_helicoptero,
+    preguntas_barcala,
+    preguntas_e3,
+    preguntas_cafeteria,
+    preguntas_asociaciones,
+    preguntas_publicaciones,
+    preguntas_e1,
+    preguntas_claustro,
+    preguntas_e2,
+    preguntas_a015,
+    preguntas_biblioteca,
 ];
 
 let params = new URLSearchParams(document.location.search);
 let estacion = params.get("estacion");
 
 function sacarPregunta(estacion) {
-        console.log(estacion);
+    let estacion_nr = est2numb.get(estacion);
 
-  if(estacion != null){
-        let estacion_nr = est2numb.get(estacion);
-        if (estacion_nr == null){
-                  window.location.replace("https://aeroespacial.da.upm.es/yincana/");
-        }
-        var preguntas = matriz_preguntas[estacion_nr]; 
-        document.getElementById("pregunta").innerHTML = preguntas[Math.floor(Math.random() * preguntas.length)];
-  }else{
-        document.getElementById("estacion").innerHTML = "Jornada de Acogida";
-        document.getElementById("estacion").innerHTML = "Delegación de Alumnos ETSIAE";
-        }
+    if (estacion_nr == null)
+        window.location.replace("https://aeroespacial.da.upm.es/yincana/");
+
+    var preguntas = matriz_preguntas[estacion_nr];
+    document.getElementById("pregunta").innerHTML = preguntas[Math.floor(Math.random() * preguntas.length)];
+
 }
-
