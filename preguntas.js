828c394771926f0a181ef3d39694209322504287
ivan-moya-o <ivan.mercurio80@gmail.com>
2025-08-03 19:19:02 +0200

    Update preguntas.js

--- preguntas.js
+++ preguntas.js
@@ -37,7 +37,7 @@ function sacarPregunta(){
     document.getElementById("pregunta").innerHTML = preguntas[Math.floor(Math.random() * preguntas.length)];
 
     if(siguiente_estacion != null){
-        document.getElementById("estacion_next").innerHTML = "</b>Próxima Estación</b>: " + "<i>" + siguiente_estacion + "</i>";
+        document.getElementById("estacion_next").innerHTML = "<b>Próxima Estación</b>: " + "<i>" + siguiente_estacion + "</i>";
     }else{
         document.getElementById("estacion_next").innerHTML = "¡Última Estación!";
     }
